f0eec856746947e0a243ea989bcdf42c3f12599b
renatoexpert <renatorraraujo@gmail.com>
2022-09-20 20:49:45 -0300

    implementing /its_ip

--- main.py
+++ main.py
@@ -1,10 +1,15 @@
 from flask import Flask, render_template
+import socket
 
 #   Serves the web-interface
 app = Flask(__name__, template_folder='web')
 @app.route("/")
 def home_page():
     return render_template('index.html')
+
+@app.route("/its_ip")
+def its_ip():
+    return socket.gethostbyname(socket.gethostname())
 
 #   "Lib": Functions that controls the GPIO
 
