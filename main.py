423ed9d4e600229fd3d89c0f5565adf90d21a167
renatoexpert <renatorraraujo@gmail.com>
2022-09-20 15:50:32 -0300

    implementing basic of flask

--- main.py
+++ main.py
@@ -1,6 +1,12 @@
 #!/bin/python3
 
+from flask import Flask
+
 #   Serves the web-interface
+app = Flask(__name__)
+@app.route("/")
+def hello_world():
+    return "<p>Hello, World!</p>"
 
 #   "Lib": Functions that controls the GPIO
 
