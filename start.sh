ef7a36b78ec4c9024d7e8b95886dcde077075b9f
renatoexpert <renatorraraujo@gmail.com>
2022-09-20 20:50:21 -0300

    run each command only if the previous were successful

--- start.sh
+++ start.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-python3 -m venv env
-. env/bin/activate
-python3 -m pip install -r requirements.txt
-flask --app main run --host=0.0.0.0
+echo 'Asuring virtual enviroment'		&& \
+python3 -m venv env				&& \
+echo 'Activating virtual enviroment'		&& \
+. env/bin/activate				&& \
+echo 'Installing dependencies'			&& \
+python3 -m pip install -r requirements.txt	&& \
+flask --app main run --host=0.0.0.0	
