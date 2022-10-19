b2671a338d013780a7346423ca0c88dad37df96e
Renato Ramos Ribeiro Araujo <renatorraraujo@gmail.com>
2022-10-19 16:03:26 -0300

    changing bin dir to /bin/janus-controller

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@ git -v || apt install git -y || pacman -S --noconfirm git
 rm -rf janus-controller	# Ensure no existing repo
 git clone https://github.com/renatoexpert/janus-controller
 mkdir -p /usr/bin/janusd 
-cp janus-controller/* -rv /usr/bin/janusd 
+cp janus-controller/* -rv /usr/bin/janus-controller 
 echo "Executables installed with success"
 
 echo "Setting up daemon..."
