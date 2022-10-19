03cd77ed9fc93a21ac493f248a7dba72e87b48af
Renato Ramos Ribeiro Araujo <renatorraraujo@gmail.com>
2022-10-19 15:45:09 -0300

    removing &&s

--- install.sh
+++ install.sh
@@ -1,14 +1,14 @@
 git -v || apt install git -y || pacman -S --noconfirm git
-mkdir /tmp/janusdownload &&\
-cd /tmp/janusdownload &&\
-git clone https://github.com/renatoexpert/janus-controller &&\
-mkdir -p /usr/bin/janusd &&\
-cp */* -rv /usr/bin/janusd &&\
+mkdir /tmp/janusdownload 
+cd /tmp/janusdownload 
+git clone https://github.com/renatoexpert/janus-controller
+mkdir -p /usr/bin/janusd 
+cp */* -rv /usr/bin/janusd 
 echo "Executables installed with success"
 
 echo "Setting up daemon..."
-cp janusd.service /etc/systemd/system/ &&\
-systemctl daemon-reload &&\
-systemctl enable janusd &&\
-systemctl start janusd &&\
+cp janusd.service /etc/systemd/system/ 
+systemctl daemon-reload 
+systemctl enable janusd 
+systemctl start janusd 
 echo "Daemon configured with success"
