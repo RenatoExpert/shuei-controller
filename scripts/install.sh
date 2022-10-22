62210871177a490a9c62d6d222badebb9a4d6953
Renato Ramos Ribeiro Araujo <renatorraraujo@gmail.com>
2022-10-22 20:48:22 -0300

    replacing janus by shuei

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,17 +1,17 @@
 git -v || apt install git -y || pacman -S --noconfirm git
-rm -rf janus-controller	# Ensure no existing repo
-git clone https://github.com/renatoexpert/janus-controller
-mkdir -p /usr/bin/janus-controller 
-cp janus-controller -rv /usr/bin/
+rm -rf shuei-controller	# Ensure no existing repo
+git clone https://github.com/renatoexpert/shuei-controller
+mkdir -p /usr/bin/shuei-controller 
+cp shuei-controller -rv /usr/bin/
 echo "Executables installed with success"
 
 pipenv --version || apt install -y python3-venv
 
 echo "Setting up daemon..."
-cp janus-controller/systemd/janusd.service /etc/systemd/system/ 
+cp shuei-controller/systemd/shueid.service /etc/systemd/system/ 
 systemctl daemon-reload 
-systemctl enable janusd 
-systemctl start janusd 
-systemctl status janusd
+systemctl enable shueid 
+systemctl start shueid 
+systemctl status shueid
 echo "Daemon configured with success"
-rm -rf janus-controller	# Cleaning
+rm -rf shuei-controller	# Cleaning
