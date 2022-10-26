7b8c1f7c74646276794ca5b412c5c87832b5fa9b
Renato Ramos Ribeiro Araujo <renatorraraujo@gmail.com>
2022-10-26 15:11:09 -0300

    Removing flask and sqlite

--- scripts/start.sh
+++ scripts/start.sh
@@ -10,4 +10,4 @@ echo 'Activating virtual enviroment'		&& \
 echo 'Installing dependencies'			&& \
 python3 -m pip install -r \
 	/bin/shuei-controller/requirements.txt	&& \
-flask --app shuei run --host=0.0.0.0
+python3 shuei
---
+++ shuei/__main__.py
@@ -0,0 +1,48 @@
+#import RPi.GPIO as GPIO
+import socket
+import os
+#from PyAccessPoint import pyaccesspoint
+
+#   First use: make wireless hotspot
+#access_point = pyaccesspoint.AccessPoint(wlan='wlan1', ssid='Janus')
+#access_point.start()
+#access_point.is_running()
+# access_point.stop()
+
+"""
+#   Setting GPIO pins
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(2,GPIO.IN)
+GPIO.setup(3,GPIO.OUT)
+GPIO.setup(23,GPIO.IN)
+GPIO.setup(24,GPIO.OUT)
+GPIO.setup(5,GPIO.IN)
+GPIO.setup(6,GPIO.OUT)
+"""
+
+#   Get uuid
+def get_uuid():
+    os.system("grep /proc/cpuinfo 'Serial'")
+
+#   Upgrade
+def upgrade():
+    command = os.system(". /bin/shuei-controller/scripts/upgrade")
+    return str(command)
+
+#   Serves the web-interface
+
+#   Keep synchronizing with Server
+port = 2000
+host = 'localhost'
+def sync():
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((host,port))
+    s.send(b'{ "uuid":"j324u", "gstatus":"333" }\n')
+    data = s.recv(1024)
+    s.send(b'0')
+    s.close()
+    print('Server', repr(data))
+
+if __name__ == "__main__":
+    sync()
+
