31a76a6d5821826a10874c812f7eb0fadf9814ef
Renato Ramos Ribeiro Araujo <renatorraraujo@gmail.com>
2022-10-29 14:08:37 -0300

    fix missing "" and false->False

--- shuei/fakegpio.py
+++ shuei/fakegpio.py
@@ -5,7 +5,7 @@ class fakegpio:
     OUT = False
     pins = {} 
     def setup (self, pin, mode):
-        self.pins[pin] = {mode:mode, state:false}
+        self.pins[pin] = {'mode':mode, 'state':False}
     def output (self, pin, state):
         if self.pins[pin][mode]: raise f'Pin {pin} is not set as output!'
         else: self.pins[pin][state] = state
